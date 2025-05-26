ec67353e659c6d499644efac044716d167ce5594
bhyerain <seunghye0307@naver.com>
2025-05-26 21:21:01 +0900

    [upload] 11weeks

---
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,36 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.entity.Member;
+import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+public class MemberController {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @GetMapping("/signup")
+    public String signUpPage() {
+        return "members/new";
+    }
+
+    @PostMapping("/join")
+    public String join(MemberForm memberForm){
+        log.info(memberForm.toString());
+        //System.out.println(memberForm.toString());
+        Member member = memberForm.toEntity();
+        log.info(member.toString());
+        //System.out.println(member.toString());
+        Member saved = memberRepository.save(member);
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
+        return "";
+
+    }
+}
